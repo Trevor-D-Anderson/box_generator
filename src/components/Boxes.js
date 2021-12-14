f138743492e7df9ddf2a43308d865885b4f35265
Trevor Anderson <tbone101@gmail.com>
2021-12-13 21:14:54 -0800

    changes

--- src/components/Boxes.js
+++ src/components/Boxes.js
@@ -4,9 +4,10 @@ const Boxes = (props) => {
   const { newBoxColor } = props;
   return (
     <div className=" flex flex-row justify-around">
-      {newBoxColor.map((color) => {
+      {newBoxColor.map((color, i) => {
         return (
           <div
+            key={i}
             className="w-24 h-24"
             style={{ backgroundColor: `${color}` }}
           ></div>
